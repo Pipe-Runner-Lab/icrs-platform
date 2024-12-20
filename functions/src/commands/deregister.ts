d99cd77fc822a4119fc59c7538fb51b658ba9893
Pipe-Runner <piperunner.in@gmail.com>
2024-12-20 22:52:48 +0100

    feat: Added a deregister command and fixed registration issues

---
+++ functions/src/commands/deregister.ts
@@ -0,0 +1,101 @@
+import type { AppCommand } from "../utils/types";
+import {
+  DE_REGISTER,
+  DE_REGISTER_SUBCOMMANDS
+} from "../constants/command-list";
+import { doesGuildMatch } from "../utils/request-processing";
+import { GAMES } from "../constants/games";
+import { APIApplicationCommandInteractionDataSubcommandOption } from "@discordjs/core";
+// TODO: @ path resolution is not working when build completes
+
+export default {
+  ...DE_REGISTER,
+  callback: async ({ interaction, db }) => {
+    if (!interaction.guild_id || !doesGuildMatch(interaction.guild_id)) {
+      return {
+        content: "This command is only available in the ICRS Discord server"
+      };
+    }
+
+    const discordUserId = interaction?.member?.user?.id;
+    if (!discordUserId) {
+      return {
+        content: "Could not find your user ID"
+      };
+    }
+
+    const subcommand = interaction.data?.options?.[0]
+      .name as DE_REGISTER_SUBCOMMANDS;
+    const subcommandData = (
+      interaction.data
+        ?.options?.[0] as unknown as APIApplicationCommandInteractionDataSubcommandOption
+    ).options;
+    if (!subcommand) {
+      return {
+        content: "Invalid subcommand"
+      };
+    }
+
+    if (!Object.values(DE_REGISTER_SUBCOMMANDS).includes(subcommand)) {
+      return {
+        content: "Invalid subcommand"
+      };
+    }
+
+    const userCollection = db.collection("users");
+    if ((await userCollection.get()).size === 0) {
+      return {
+        content: "You have not registered any profiles"
+      };
+    }
+
+    /* ------------------------------- DELETE USER ------------------------------ */
+    const userDoc = await userCollection.doc(`${discordUserId}`).get();
+
+    if (!userDoc.exists) {
+      return {
+        content: "You have not registered any profile in our database"
+      };
+    }
+
+    if (subcommand === DE_REGISTER_SUBCOMMANDS.user) {
+      await userCollection.doc(`${discordUserId}`).delete();
+      return {
+        content: "Your profile has been removed"
+      };
+    }
+
+    /* -------------------------------- DELETE ID ------------------------------- */
+
+    const game = subcommandData?.[0].value as string;
+    if (!game || !Object.values(GAMES).includes(game as GAMES)) {
+      return {
+        content: "Please select a game we support"
+      };
+    }
+
+    const id = subcommandData?.[1].value as string;
+    // TODO: Implement a regex for ID validation
+    if (!id) {
+      return {
+        content: "Please provide a valid ID"
+      };
+    }
+
+    const existingIds = (userDoc.exists ? userDoc.data()?.[game] : []) ?? [];
+
+    if (existingIds.includes(id)) {
+      await userCollection.doc(`${discordUserId}`).update({
+        [game]: existingIds.filter((existingId: string) => existingId !== id)
+      });
+
+      return {
+        content: `ID ${id} has been removed from your profile`
+      };
+    } else {
+      return {
+        content: `ID ${id} not found in your profile for ${game}`
+      };
+    }
+  }
+} satisfies AppCommand;
--- functions/src/constants/command-list.ts
+++ functions/src/constants/command-list.ts
@@ -29,6 +29,71 @@ export const REGISTER = {
   ]
 } satisfies PartialAPIApplicationCommand;
 
+export enum DE_REGISTER_SUBCOMMANDS {
+  user = "user",
+  id = "id"
+}
+
+export const DE_REGISTER = {
+  name: "deregister",
+  description: "Remove profile from our database",
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    {
+      name: DE_REGISTER_SUBCOMMANDS.user,
+      description: "Deregister your entire profile",
+      type: ApplicationCommandOptionType.Subcommand
+    },
+    {
+      name: DE_REGISTER_SUBCOMMANDS.id,
+      description: "Deregister a game ID from your profile",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "game",
+          type: ApplicationCommandOptionType.String,
+          description: "Select the game you want to deregister for",
+          choices: [
+            { name: "Age Of Empires 4", value: GAMES.AOE4 },
+            { name: "Dota 2", value: GAMES.DOTA2 }
+          ],
+          required: true
+        },
+        {
+          name: "id",
+          type: ApplicationCommandOptionType.String,
+          description: "Profile ID (Check /help if you are unsure)",
+          required: true
+        }
+      ]
+    }
+  ]
+} satisfies PartialAPIApplicationCommand;
+
+export const HELP = {
+  name: "help",
+  description: "Provides usage guides and help for the bot",
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    // {
+    //   name: "type",
+    //   type: ApplicationCommandOptionType.Subcommand,
+    //   description: "Show help for a specific command",
+    //   required: true
+    // },
+    {
+      name: "game",
+      type: ApplicationCommandOptionType.String,
+      description: "Select the game you want help with",
+      choices: [
+        { name: "Age Of Empires 4", value: GAMES.AOE4 },
+        { name: "Dota 2", value: GAMES.DOTA2 }
+      ],
+      required: true
+    }
+  ]
+} satisfies PartialAPIApplicationCommand;
+
 export const LEADERBOARD = {
   name: "leaderboard",
   description: "Display our server leaderboard",
