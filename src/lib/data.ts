dc83123b1e6eb40807ed38809e57025b5d816fe9
Pipe-Runner <piperunner.in@gmail.com>
2025-03-21 02:48:41 +0100

    Created event page

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,94 +1,62 @@
 import { colors } from "./colors";
 
+interface Sponsor {
+  name: string;
+  logo: string;
+}
+
+interface Patron {
+  name: string;
+  amount: number;
+}
+
+
 export interface Event {
   id: string;
   title: string;
   color: (typeof colors)[keyof typeof colors];
   cover: string;
-  artists: string[];
   description: string;
-  date: string;
+  date: Date;
+  registrationLink?: string;
+  game: 'Age of Empires' | 'DOTA2';
+  eventHighlights?: string[];
+  additionalInfo?: string[];
+  sponsors?: Sponsor[];
+  patrons?: Patron[];
+  youtubeLink?: string;
+  twitchLink?: string;
 }
 
 export const events: Event[] = [
   {
     id: "1",
-    title: "Sound Pulse",
+    title: "ICRS // Age of Empires // Free For All",
     color: colors.teal,
     cover:
-      "/event1.jpg",
-    artists: ["Lincoln Center, NY"],
-    description: "Experience the ultimate concert event, where music comes alive and memories are made. Join us for an unforgettable...",
-    date: "May 14-15, 2023 01:00 - 05:00 PM",
-  },
-  {
-    id: "2",
-    title: "Groove Summit",
-    color: colors.green,
-    cover:
-      "/event2.jpg",
-    artists: ["Hollywood Bowl, Los Angeles"],
-    description: "Experience the ultimate concert event, where music comes alive and memories are made. Join us for an unforgettable...",
-    date: "May 14-15, 2023 01:00 - 05:00 PM",
-  },
-  {
-    id: "3",
-    title: "Vibe Wave",
-    color: colors.rose,
-    cover:
-      "/event4.jpg",
-    artists: ["Kennedy Center, Washington"],
-    description: "Experience the ultimate concert event, where music comes alive and memories are made. Join us for an unforgettable...",
-    date: "May 14-15, 2023 01:00 - 05:00 PM",
-  },
-  {
-    id: "4",
-    title: "Rhythm Revival",
-    color: colors.red,
-    cover:
-      "/event3.jpg",
-    artists: ["Symphony Hall, New York"],
-    description: "Experience the ultimate concert event, where music comes alive and memories are made. Join us for an unforgettable...",
-    date: "May 14-15, 2023 01:00 - 05:00 PM",
-  },
-  {
-    id: "5",
-    title: "Roots Revelry",
-    color: colors.pink,
-    cover:
-      "/event5.jpg",
-    artists: ["Millennium Park, Chicago"],
-    description: "Experience the ultimate concert event, where music comes alive and memories are made. Join us for an unforgettable...",
-    date: "May 14-15, 2023 01:00 - 05:00 PM",
-  },
-  {
-    id: "6",
-    title: "Cultural Mosaic",
-    color: colors.pink,
-    cover:
-      "/event6.jpg",
-    artists: ["The Fox Theatre, GA"],
-    description: "Experience the ultimate concert event, where music comes alive and memories are made. Join us for an unforgettable...",
-    date: "May 14-15, 2023 01:00 - 05:00 PM",
-  },
-];
-
-export const morePlaylists = [
-  ...events.map((item) => ({
-    ...item,
-    id: item.id + "a",
-  })),
-];
-
-export const sidebarPlaylists = [
-  ...events.map((item) => ({
-    ...item,
-    id: item.id + "_side",
-  })),
-];
-
-export const allPlaylists = [
-  ...events,
-  ...morePlaylists,
+      "https://media.wired.com/photos/6172e44f8acfc2aaa8454fbe/master/w_2240,c_limit/Age%20of%20Empires%20IV_3_4K.jpg",
+    description: "Welcome to the first ICRS event of the year! We will be playing Age of Empires in a Free For All format. Top 3 players will receive a prize!",
+    date: new Date("2021-09-01"),
+    registrationLink: "https://forms.gle/3g4Qv9W9Qq4jF4uG6",
+    game: 'Age of Empires',
+    eventHighlights: [
+      "Free For All",
+      "Top 3 players receive a prize",
+    ],
+    additionalInfo: [
+      "The event will be streamed on our Twitch channel",
+      "The event will be hosted on our Discord server",
+    ]
+    ,
+    sponsors: [
+      {
+        name: "Game Syndrome",
+        logo: "https://lh3.googleusercontent.com/a-/ALV-UjXlTsMdTpRcWiqvGClQT6xQ3lO6uh4KwjKozXIksFE05XJH2xM=w60-h60-p-rp-mo-br100",
+      }
+    ],
+    patrons: [
+      {name:"Pipe Runner", amount: 100},
+    ],
+  }
 ];
 
---
+++ src/lib/links.ts
@@ -0,0 +1,3 @@
+export const DiscordLink = "https://discord.gg/invite";
+export const YoutubeLink = "https://instagram.com/invite";
+export const TwitchLink = "https://twitter.com/invite";
